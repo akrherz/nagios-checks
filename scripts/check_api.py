6a5d02b4b5d9e50944ca323ae1d21ad5e0b55661
akrherz <akrherz@iastate.edu>
2020-08-03 11:24:14 -0500

    add API check

---
+++ scripts/check_api.py
@@ -0,0 +1,32 @@
+"""Ensure that /api/ is returning results."""
+import sys
+from datetime import datetime
+
+import requests
+
+ENDPOINTS = {"MOS": "/api/1/mos.txt?station=KDSM&model=GFS"}
+
+
+def main(argv):
+    """Go Main Go."""
+    uri = f"http://iem.local{ENDPOINTS[argv[1]]}"
+    sts = datetime.utcnow()
+    try:
+        req = requests.get(uri, timeout=20)
+        status_code = req.status_code
+    except Exception:
+        status_code = 999
+    ets = datetime.utcnow()
+
+    msg = "%s: %s %s |timing=%.3fs;10;5;3" % (
+        "OK" if status_code == 200 else "CRITICAL",
+        status_code,
+        uri,
+        (ets - sts).total_seconds(),
+    )
+    print(msg)
+    return 0 if status_code == 200 else 2
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
