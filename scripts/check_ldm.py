07b0f0af7f02af0fbe1aada16b1fa918de50ba43
akrherz <akrherz@iastate.edu>
2022-07-06 11:23:14 -0500

    add more LDM metrics from pqmon

--- scripts/check_ldm.py
+++ scripts/check_ldm.py
@@ -12,25 +12,50 @@ getTime(), getLoad(), getPortCount(), getPq(), getCpu()
 24137944e3 21514 $swapFree, $contextSwitches
 
 """
-from __future__ import print_function
 import subprocess
 import sys
 import os
 
 
-def main():
-    """Go Main Go."""
+def find_ldmpath():
+    """Figure out where LDM is."""
     for username in ["ldm", "meteor_ldm"]:
         fn = f"/home/{username}/bin/ldmadmin"
-        if not os.path.isfile(fn):
-            continue
-        proc = subprocess.Popen([fn, "printmetrics"], stdout=subprocess.PIPE)
-        break
-    data = proc.stdout.read().decode("ascii")
+        if os.path.isfile(fn):
+            return f"/home/{username}/bin"
+    return None
+
+
+def main():
+    """Go Main Go."""
+    ldmpath = find_ldmpath()
+    with subprocess.Popen(
+        [f"{ldmpath}/ldmadmin", "printmetrics"],
+        stdout=subprocess.PIPE,
+    ) as proc:
+        data = proc.stdout.read().decode("ascii").strip()
     tokens = data.split()
     if len(tokens) != 18:
         print(f"CRITICAL - can not parse output {data} ")
         sys.exit(2)
+
+    # Get pqmon stats
+    with subprocess.Popen(
+        [f"{ldmpath}/pqmon", "-S"],
+        stdout=subprocess.PIPE,
+    ) as proc:
+        data = proc.stdout.read().decode("ascii").strip()
+    pqmon_tokens = data.split()
+    if len(pqmon_tokens) != 12:
+        print(f"CRITICAL - can not parse pqmon output {data} ")
+        sys.exit(2)
+
+    queue_size_total = int(pqmon_tokens[1])
+    queue_size_used = int(pqmon_tokens[2])
+    queue_size_util = queue_size_used / queue_size_total * 100.0
+    queue_slots_total = int(pqmon_tokens[4])
+    queue_slots_used = int(pqmon_tokens[5])
+    queue_slots_util = queue_slots_used / queue_slots_total * 100.0
 
     # We are downstream of how many hosts
     downstream = int(tokens[4])
@@ -42,15 +67,21 @@ def main():
 
     msg = "OK"
     estatus = 0
-    # CHECK that the registry has a valid netstat command not `true`!
     if downstream < 1:
         msg = "CRITICAL"
         estatus = 2
+    # Ideally, the LDM queue is close to full on size, but still has some
+    # slots available, so if slots > 95% and size < 75%, we have trouble
+    if queue_size_util < 75 and queue_slots_util > 95:
+        msg = "CRITICAL Slot Exhaustion"
+        estatus = 2
+
     print(
         f"{msg} - Down:{downstream} Up:{upstream} Raw:{data}| "
         f"downstream={downstream};; upstream={upstream};; "
         f"queue_age={queue_age};; product_count={product_count};; "
-        f"byte_count={byte_count};;"
+        f"byte_count={byte_count};; queue_slot_util={queue_slots_util:.3f};; "
+        f"queue_size_util={queue_size_util:.3f};;"
     )
     return estatus
 
