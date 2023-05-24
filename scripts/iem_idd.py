39f1ce0ec253ebecd9e9d9c78de4f678660faf53
akrherz <akrherz@iastate.edu>
2023-05-24 08:52:39 -0500

    piggyback JSON check

--- scripts/iem_idd.py
+++ scripts/iem_idd.py
@@ -2,6 +2,7 @@
  Nagios check to make sure we have data flowing through LDM
 """
 import datetime
+import json
 import os
 import stat
 import sys
@@ -9,10 +10,17 @@ import sys
 
 def main():
     """Go Main Go."""
-    FN = "/mesonet/ldmdata/gis/images/4326/USCOMP/n0q_0.png"
-    now = datetime.datetime.now()
-    mtime = os.stat(FN)[stat.ST_MTIME]
-    ts = datetime.datetime.fromtimestamp(mtime)
+    fn = "/mesonet/ldmdata/gis/images/4326/USCOMP/n0q_0.png"
+    # Ensure that our JSON file is working too per 23 May 2023 explosion
+    with open(fn.replace("png", "json"), encoding="ascii") as fh:
+        data = json.load(fh)
+    fmt = "%Y-%m-%dT%H:%M:%SZ"
+    valid = datetime.datetime.strptime(data["meta"]["valid"], fmt)
+    valid = valid.replace(tzinfo=datetime.timezone.utc)
+    now = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
+    mtime = os.stat(fn)[stat.ST_MTIME]
+    ts = datetime.datetime.fromtimestamp(mtime, tz=datetime.timezone.utc)
+    ts = min([ts, valid])
     diff = (now - ts).days * 86400.0 + (now - ts).seconds
     if diff < 600:
         print(f"OK - n0q_0.png {ts}")
