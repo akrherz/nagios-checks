7ac4096992f8b22d4c1401647e9ffb7e3363cf61
akrherz <akrherz@iastate.edu>
2023-10-18 19:51:43 -0500

    add OK percentage

--- scripts/check_telemetry.py
+++ scripts/check_telemetry.py
@@ -9,17 +9,19 @@ def main():
     """Go Main Go"""
     with get_sqlalchemy_conn("mesosite", user="nobody") as conn:
         df = pd.read_sql(
-            "select timing from website_telemetry "
+            "select timing, status_code from website_telemetry "
             "where valid > now() - '5 minutes'::interval",
             conn,
         )
+    ok = len(df[df["status_code"] == 200].index) / len(df.index) * 100.0
     levels = df["timing"].quantile([0.5, 0.95, 0.99])
 
     print(
-        f"Telemetry rate:{len(df.index) / 300.:.4f} "
+        f"Telemetry OK: {ok:.2f}% rate:{len(df.index) / 300.:.4f} "
         f"mean:{levels[0.50]:.5f} | "
         f"RATE={len(df.index) / 300.:.4f};; MEAN={levels[0.5]:.5f};; "
-        f"P95={levels[0.95]:.5f};; P99={levels[0.99]:.5f};;"
+        f"P95={levels[0.95]:.5f};; P99={levels[0.99]:.5f};; "
+        f"OK={ok:.5f};;"
     )
     return 0
 
