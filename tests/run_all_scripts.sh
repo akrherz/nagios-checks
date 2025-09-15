d3d4b85261ac376ecd32a6642849c310b9e15338
akrherz <akrherz@iastate.edu>
2025-09-15 09:22:41 -0500

    📝 Tweak hoping to see a GHA failure

--- tests/run_all_scripts.sh
+++ tests/run_all_scripts.sh
@@ -1,8 +1,15 @@
 # Run every script in scripts and ensure an exit status of 0, 1, or 2
+set -x
 
-for script in scripts/*; do
+for script in scripts/*.py; do
     echo "Running $script"
-    python $script
+    # Run the script and capture the STDERR to a variable called se
+    se=$(python "$script" 2>&1)
+    # If "Traceback" is found in se, we have a problem
+    if echo "$se" | grep -q "Traceback"; then
+        echo "Script $script produced a traceback:"
+        exit 1
+    fi
     status=$?
     if [[ $status -ne 0 && $status -ne 1 && $status -ne 2 ]]; then
         echo "Script $script exited with unexpected status $status"
